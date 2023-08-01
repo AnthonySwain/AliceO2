814162344048db2867a5a327a7cc5c818991f284
Benedikt Volkel <benedikt.volkel@cern.ch>
2023-08-01 10:11:31 +0200

    Tidy macro

--- TonysDevelopmentArea/macros/DumpHashMaps.C
+++ TonysDevelopmentArea/macros/DumpHashMaps.C
@@ -1,22 +1,51 @@
+#if defined(__linux__)
+R__ADD_INCLUDE_PATH($VECGEOM_ROOT/../../Vc/latest/include)
+#endif
 
 R__ADD_INCLUDE_PATH($VECGEOM_ROOT/include)
-#include <VecGeom/base/FlatVoxelHashMap.h> 
 
-vecgeom::Vector3D<float> FindVoxelCenter(float x,float y, float z){
+#if defined(__linux__)
+#include <VecCore/Types.h>
+#include <VecCore/Common.h>
+#include <VecCore/Backend/Implementation.h>
+#include <VecCore/Backend/Interface.h>
+#include <VecCore/Backend/VcVector.h>
+#endif
+
+#include <VecGeom/base/FlatVoxelHashMap.h>
+
+template <typename P, bool ScalarProperties>
+vecgeom::Vector3D<float> FindVoxelCenter(vecgeom::FlatVoxelHashMap<P, ScalarProperties>* VoxelMap, float x,float y, float z)
+{
   vecgeom::Vector3D<float> pos(x, y, z);
   auto key = VoxelMap->getVoxelKey(pos);
-  return (VoxelMap->keyToPos(key));   
+  return VoxelMap->keyToPos(key);
 }
 
-bool VoxelCheck(float x,float y, float z){
+template <typename P, bool ScalarProperties>
+bool VoxelCheck(vecgeom::FlatVoxelHashMap<P, ScalarProperties>* VoxelMap, float x,float y, float z)
+{
   vecgeom::Vector3D<float> pos(x, y, z);
   //auto key = VoxelMap->getVoxelKey(pos);
-  if ((VoxelMap)->isOccupied(pos)){
-    std::cout << "IS BLACKHOLE Particle Deleted, POSITION: " << x << ", " << y << ", " << z << "\n"; 
-    return(true);
+  if (VoxelMap->isOccupied(pos)){
+    std::cout << "IS BLACKHOLE Particle Deleted, POSITION: " << x << ", " << y << ", " << z << "\n";
+    return true;
   }
+  return false;
+}
 
-  return(false);
+template <typename P, bool ScalarProperties>
+void AssignVoxelTrue(vecgeom::FlatVoxelHashMap<P, ScalarProperties>* VoxelMap, float x, float y, float z)
+{
+  vecgeom::Vector3D<float> pos(x, y, z);
+  //auto key = (VoxelMap)->getVoxelKey(pos);
+
+  //If its already been set to true, don't touch:)
+  if (VoxelCheck(VoxelMap, x,y,z)) {
+    return;
+  }
+  VoxelMap->addProperty(pos, true);
+  std::cout << "BLACKHOLE Set, POSITION: " << x << ", " << y << ", " << z << "\n";
 }
 
 float UniformRandom(float upper_bound, float lower_bound){
@@ -33,66 +62,57 @@ float UniformRandom(float upper_bound, float lower_bound){
   return(random_number);
 }
 
-void RandomAllocation(int N, float Min, float Max){
+template <typename P, bool ScalarProperties>
+void RandomAllocation(vecgeom::FlatVoxelHashMap<P, ScalarProperties>* VoxelMap, int N, float Min, float Max){
   //Assign N random voxels near the center as true (i.e blachholes )
   for (int i =0; i < N; i++){
   float x = UniformRandom(Min,Max);
   float y = UniformRandom(Min,Max);
   float z = UniformRandom(Min,Max);
 
-  AssignVoxelTrue(x,y,z);
+  AssignVoxelTrue(VoxelMap, x, y, z);
   }
 }
 
-void AssignVoxelTrue(float x, float y, float z){  
-  vecgeom::Vector3D<float> pos(x, y, z);
-  //auto key = (VoxelMap)->getVoxelKey(pos);
-
-  //If its already been set to true, don't touch:) 
-  if (VoxelCheck(x,y,z)){}
-  
-  else{
-  VoxelMap->addProperty(pos, true);
-  std::cout << "BLACKHOLE Set, POSITION: " << x << ", " << y << ", " << z << "\n";
-  }}
-
-void BuildWallZYplane(float Xval, int thickness, int Zmin, int Zmax, int Ymin, int Ymax, int ZBins, int YBins){
+template <typename P, bool ScalarProperties>
+void BuildWallZYplane(vecgeom::FlatVoxelHashMap<P, ScalarProperties>* VoxelMap, float Xval, int thickness, int Zmin, int Zmax, int Ymin, int Ymax, int ZBins, int YBins){
   float deltaY = (Ymax - Ymin)/YBins;
   float deltaZ = (Zmax - Zmin)/ZBins;
 
   for (float k = 0; k < thickness ;k++){
   for (float j = (float(Ymin) + deltaY/2); j < float(Ymax); j += deltaY ){
     for (float i = (float(Zmin) + deltaZ/2); i < float(Zmax); i += deltaZ ){
-      AssignVoxelTrue(Xval+k, j , i);
+      AssignVoxelTrue(VoxelMap, Xval+k, j, i);
   }}
   }
 }
 
-void BuildWallXYplane(float Zval, int thickness, int Xmin, int Xmax, int Ymin, int Ymax, int XBins, int YBins){
+template <typename P, bool ScalarProperties>
+void BuildWallXYplane(vecgeom::FlatVoxelHashMap<P, ScalarProperties>* VoxelMap, float Zval, int thickness, int Xmin, int Xmax, int Ymin, int Ymax, int XBins, int YBins){
   float deltaY = (Ymax - Ymin)/YBins;
   float deltaX = (Xmax - Xmin)/XBins;
 
   for (float k = 0; k<thickness; k++){
   for (float j = (float(Ymin) + deltaY/2); j < float(Ymax); j += deltaY ){
     for (float i = (float(Xmin) + deltaX/2); i < float(Xmax); i += deltaX ){
-      AssignVoxelTrue(i, j , Zval+k);
+      AssignVoxelTrue(VoxelMap, i, j, Zval+k);
   }}
   }
 }
 
-void BuildWallXZplane(float Yval, int thickness, int Xmin, int Xmax, int Zmin, int Zmax, int XBins, int ZBins){
+template <typename P, bool ScalarProperties>
+void BuildWallXZplane(vecgeom::FlatVoxelHashMap<P, ScalarProperties>* VoxelMap, float Yval, int thickness, int Xmin, int Xmax, int Zmin, int Zmax, int XBins, int ZBins){
   float deltaZ = (Zmax - Zmin)/ZBins;
   float deltaX = (Xmax - Xmin)/XBins;
 
   for (float k = 0; k<thickness; k++){
-  for (float j = (float(Zmin) + deltaZ/2); j < float(Zmax); j += deltaZ ){
-    for (float i = (float(Xmin) + deltaX/2); i < float(Xmax); i += deltaX ){
-      AssignVoxelTrue(i, Yval +k, j);
-  }}
+    for (float j = (float(Zmin) + deltaZ/2); j < float(Zmax); j += deltaZ ){
+      for (float i = (float(Xmin) + deltaX/2); i < float(Xmax); i += deltaX ){
+        AssignVoxelTrue(VoxelMap, i, Yval +k, j);
+      }
+    }
   }
 }
-
-
 
 
 void DumpHashMaps(){
@@ -100,13 +120,13 @@ void DumpHashMaps(){
 
     vecgeom::Vector3D<float> MinValues(-1000,-1000,-3000);
     vecgeom::Vector3D<float> Lengths(2000,2000,6000);
-    int NumbBins[3] = {2000,2000,6000}; 
+    int NumbBins[3] = {2000,2000,6000};
     std::unique_ptr<vecgeom::FlatVoxelHashMap<bool,true>>VoxelMap = std::make_unique<vecgeom::FlatVoxelHashMap<bool,true>>(MinValues, Lengths, NumbBins[0],NumbBins[1],NumbBins[2]);
 
     //Assign some voxels as true
-    AssignVoxelTrue(0,0,0);
+    AssignVoxelTrue(VoxelMap.get(), 0,0,0);
 
     //Dump to file
-    VoxelMap->dumpToTFile("HashMap1.root"); 
+    VoxelMap->dumpToTFile("HashMap1.root");
 
 }
